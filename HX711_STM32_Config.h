bcfdc98cb39e77218627207180abb40114a8937d
BHD <49844717+bhd0y8@users.noreply.github.com>
2021-06-02 21:53:33 +0430

    HX711_STM32 Library commit_2

--- HX711_STM32_Config.h
+++ HX711_STM32_Config.h
@@ -3,11 +3,11 @@
 
 		#include <stdint.h>
 
-		#define 				_HX711_SCK_GPIO     	  GPIOB
-		#define 				_HX711_SCK_PIN      		GPIO_PIN_1
-		#define 				_HX711_DI_GPIO      		GPIOB
-		#define 				_HX711_DI_PIN     			GPIO_PIN_0
-		#define 				_HX711_USE_FREERTOS     0
+		#define 				_HX711_SCK_GPIO     	   GPIOB
+		#define 				_HX711_SCK_PIN      	   GPIO_PIN_1
+		#define 				_HX711_DI_GPIO         	   GPIOB
+		#define 				_HX711_DI_PIN    	   GPIO_PIN_0
+		#define 				_HX711_USE_FREERTOS        0
 
 
 		#endif
